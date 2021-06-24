d89c73ee02ac89225b5134fcfd9ab957501c975b
buran5884 <buran5884@gmail.com>
2021-06-24 14:39:55 +0900

    edit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "img_handler.hpp"
+#include "strahl.hpp"
 
 using namespace std;
 
-#define HEIGHT 480
-#define WIDTH 720
+#define HEIGHT 10
+#define WIDTH 10
 #define CHANNEL_NUM 3
 
 int main() {
     RenderedImg img(WIDTH, HEIGHT, MODE_RGB);
-    img.FillColor({ 255, 255, 255 });
-    img.SetPixelColor(9, 9, { 0, 255, 255 });
+    RGBA fillcolor(0, 0, 0);
+    RGBA pixelcolor(0, 255, 255);
+    img.FillColor(fillcolor);
+    img.SetPixelColor(9, 9, pixelcolor);
     img.SaveImg("result.png");
 
     return 0;
