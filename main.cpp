52515277b268789435753280820846c8ecad55cf
kobayashi <rkoba98@stu.kanazawa-u.ac.jp>
2021-07-21 11:54:10 +0900

    add scene

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 int main() {
     Object3D monkey;
-    monkey.ImportOBJ("3dObjects/monkey.obj");
+    monkey.ImportOBJ("3dObjects/scene.obj");
 
     Scene scene;
     scene.AddObject(monkey, "monkey01");
-    scene.SetObjectRotation("monkey01", vector3d(0, 0, 90));
-    scene.SetObjectLocation("monkey01", vector3d(0, 0, -3.0));
+    // scene.SetObjectRotation("monkey01", vector3d(0, 0, 90));
+    // scene.SetObjectLocation("monkey01", vector3d(0, 0, -3.0));
 
     CameraPers cam(60.0, WIDTH, HEIGHT);
     cam.Render(scene, "result.ppm");
